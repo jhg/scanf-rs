46a93c425af4b70a5f7fb250e69eb89fb5603545
Jesus Hernandez <jesushdez@protonmail.com>
2025-08-17 01:00:58 +0200

    Remove deprecated format module and related error handling code

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,5 @@
 #![allow(clippy::needless_return)]
 #![doc = include_str!("../README.md")]
 
-#[doc(hidden)]
-pub mod format;
-
 // Re-export the procedural macros
 pub use scanf_proc_macro::{scanf, sscanf};
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-}
